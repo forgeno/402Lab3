02449653c358a4707754f67cc9c7bf809406c22b
forgeno <ima@ualberta.ca>
2019-02-06 20:05:15 -0500

    fixed commit

--- src/main/java/cmput402/tdd/Utility.java
+++ src/main/java/cmput402/tdd/Utility.java
@@ -1,5 +1,6 @@
 package cmput402.tdd;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import cmput402.tdd.service.Search;
@@ -7,12 +8,18 @@ import cmput402.tdd.service.Search;
 public class Utility {
 
 	public HashMap<String, Float>  countAverageScore(Search search){
+		HashMap<String,ArrayList<Integer>> results = search.returnAll();
+		HashMap<String, Float> map = new HashMap<String, Float>();
+		for(String player: results.keySet()){
+			float avg = (float)(results.get(player).get(1)/results.get(player).get(0));
+			map.put(player, avg);//hello
 		
-		return null;
+		}
+		return map;
 	}
 	
 public HashMap<String, Integer>  findTopScorer(Search search){
-		
+		// findTopScorer(Search search);
 		return null;
 	}
 	
